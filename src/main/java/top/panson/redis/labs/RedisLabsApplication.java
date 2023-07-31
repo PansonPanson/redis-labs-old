779e6f2fca2c7bcaabc23e27ce4251f6e2126b61
Panson <panxing@flashhold.com>
2023-07-31 16:13:07 +0800

    init

---
+++ src/main/java/top/panson/redis/labs/RedisLabsApplication.java
@@ -0,0 +1,16 @@
+package top.panson.redis.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @create 2023-07-31 15:39
+ * @Author: Panson
+ */
+@SpringBootApplication
+public class RedisLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLabsApplication.class, args);
+    }
+}
